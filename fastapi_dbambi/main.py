d38a64b708489fac8d126e4af19500e1403aadf1
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-09-01 21:31:28 -0600

    swap out return strings for jinja2 tempaltes

--- fastapi_dbambi/main.py
+++ fastapi_dbambi/main.py
@@ -1,7 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+# from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
+templates = Jinja2Templates(directory = "templates")
+
+posts: list[dict] = [
+  {
+    "id" : 1,
+    "author" : "Corey Schafer",
+    "title" : "FastAPI is Awesome",
+    "content" : "This framework is really easy to use and super fast.",
+    "date_posted" : "April 20, 2025",
+  },
+  {
+    "id" : 2,
+    "author" : "Jane Doe",
+    "title" : "Python is Great for Web Development",
+    "content" : "Python is a great language for web development,  and FastAPI makes if even better.",
+    "date_posted" : "April 21, 2025",
+  }
+]
+
+# @app.get("/")
+# def home():
+#     return {"Message" : "Hello world payload"}
+
+# include_in_schema -> add route to docs page when true, exclude when false
+# @app.get("/", response_class = HTMLResponse, include_in_schema = True)
+# @app.get("/posts", response_class = HTMLResponse, include_in_schema = False)
+# def home():
+#     return f"<h1>{posts[0]['title']}</h1>"
+
+@app.get("/posts", response_class = HTMLResponse, include_in_schema = False)
 def home():
-    return {"Message" : "Hello world payload"}
+    return f"<h1>{posts[0]['title']}</h1>"
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
