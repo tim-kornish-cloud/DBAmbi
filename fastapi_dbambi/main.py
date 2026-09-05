c16953333b6c08a744394e215325f30eac5c74a0
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-09-04 23:58:58 -0600

    adding html for specific posts and error feedback

--- fastapi_dbambi/main.py
+++ fastapi_dbambi/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request, status
 from fastapi.staticfiles import StaticFiles
 # from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -55,4 +55,4 @@ def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
             return post
-    return {"error" : "Post Not found"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
