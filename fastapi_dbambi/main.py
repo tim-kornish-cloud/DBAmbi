c00391c56b08adae25e9a7b5d3e2798ae92f4eab
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-09-04 23:43:05 -0600

    add dynamic routing

--- fastapi_dbambi/main.py
+++ fastapi_dbambi/main.py
@@ -49,3 +49,10 @@ def home(request: Request):
 @app.get("/api/posts")
 def get_posts():
     return posts
+
+@app.get("/api/posts/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    return {"error" : "Post Not found"}
