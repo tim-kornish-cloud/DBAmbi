19a9e30bcf041740de2c4bc85ac04f6bbecae36d
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-09-05 22:54:08 -0600

    add post specific route with post.html

--- fastapi_dbambi/main.py
+++ fastapi_dbambi/main.py
@@ -46,6 +46,14 @@ posts: list[dict] = [
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts" : posts, "title" : "Home"})
 
+@app.get("/posts/{post_id}", include_in_schema = False,)
+def get_post(request: Request, post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            title = post['title'][:50]
+            return templates.TemplateResponse(request, "post.html", {"post" : post, "title" : title})
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Post not found")
+
 @app.get("/api/posts")
 def get_posts():
     return posts
@@ -55,4 +63,4 @@ def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
             return post
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Post not found")
