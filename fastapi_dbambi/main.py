d3cbc9a3909859e785e4bbf40719ce0453738f1d
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-09-13 23:53:49 -0600

    add put request to update post

--- fastapi_dbambi/main.py
+++ fastapi_dbambi/main.py
@@ -11,7 +11,7 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 
 import models
 from database import Base, engine, get_db
-from schemas import PostCreate, PostResponse, UserCreate, UserResponse
+from schemas import PostCreate, PostUpdate, PostResponse, UserCreate, UserResponse
 
 Base.metadata.create_all(bind=engine)
 
@@ -168,6 +168,30 @@ def get_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
     if post:
         return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
+@app.put("/api/posts/{post_id}", response_model=PostResponse)
+def update_post_full(post_id: int, post_data: PostCreate, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+    if post_data.user_id != post.user_id:
+        result = db.execute(select(models.User).where(models.User.id == post_data.user_id))
+        user = result.scalars().first()
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User not found",
+            )
+    post.title = post_data.title
+    post.content = post_data.content
+    post.user_id = psot_data.user_id
+
+    db.commit()
+    db.refresh()
+    return post
 
 
 @app.exception_handler(StarletteHTTPException)
