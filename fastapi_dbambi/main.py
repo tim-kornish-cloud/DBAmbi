fff5894bcfbd00328e97689b86ba6052241c9ad2
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-09-25 22:02:52 -0600

    add login and register routes

--- fastapi_dbambi/main.py
+++ fastapi_dbambi/main.py
@@ -98,6 +98,22 @@ async def user_posts_page(
         {"posts": posts, "user": user, "title": f"{user.username}'s Posts"},
     )
 
+@app.get("/login", include_in_schema=False)
+async def login_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "login.html",
+        {"title": "Login"},
+    )
+
+
+@app.get("/register", include_in_schema=False)
+async def register_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "register.html",
+        {"title": "Register"},
+    )
 
 @app.exception_handler(StarletteHTTPException)
 async def general_http_exception_handler(
